b275d228c57221a571bf286c1e31a9e4e475f8c2
carolzelima <carol.gha@gmail.com>
2022-01-20 21:38:10 -0300

    initial files

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = "Carolina";
+console.log(name)
+let number = 10;
+console.log(number)
+let currentYear = 2022
+console.log (currentYear)
+console.log(currentYear - number)
+console.log(currentYear + number)
+
+console.log ("Estou mexendo no console.log")
+
+let quartaFeira = true;
+console.log(quartaFeira)
